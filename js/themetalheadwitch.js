d66ccb9c9f0e88ecdb1e5fede8d10f475133b619
Serafina Cordero <serafina.cordero@uconn.edu>
2023-03-27 12:15:43 -0400

    fixed nav and footer
    adjusted categories, added disclaimer to footer

--- js/themetalheadwitch.js
+++ js/themetalheadwitch.js
@@ -1,10 +1,23 @@
 var a_parent = document.querySelectorAll(".a_parent");
+var submenu_b = document.querySelectorAll(".submenu_b");
 
 a_parent.forEach(function(a_parent_item) {
     a_parent_item.addEventListener("click", function(){
+        submenu_b.forEach(function(submenu_b_item){
+            submenu_b_item.classList.remove("active");
+        })
         a_parent.forEach(function(a_parent_item){
             a_parent_item.classList.remove("active");
         })
         a_parent_item.classList.add("active");
+    })
+})
+
+submenu_b.forEach(function(submenu_b_item) {
+    submenu_b_item.addEventListener("click", function(){
+        submenu_b.forEach(function(submenu_b_item){
+            submenu_b_item.classList.remove("active");
+        })
+        submenu_b_item.classList.add("active");
     })
 })
